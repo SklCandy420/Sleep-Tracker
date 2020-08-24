070117f7c112f54fb4e9f1cb5dd25b00a61fd0a5
SklCandy420 <43821102+SklCandy420@users.noreply.github.com>
2020-08-24 12:48:58 +0530

    strings added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,6 +19,7 @@ void main() {
 
  class MyHomepage extends StatefulWidget {
   MyHomepage({Key key,this.title}):super(key:key);
+
   final String title;
 
    @override
@@ -24,10 +27,53 @@ void main() {
  }
 
  class _MyHomepageState extends State<MyHomepage> {
-   @override
+
+  TextEditingController _c;
+  double _opacity =0.0;
+  var cycles = {'firstCycle':'','secondCycle':'', 'thirdCycle':'', 'forthCycle':'', 'fifthCycle':'', 'sixthCycle':''};
+
+
+  @override
+  void initState() {
+
+    _c = new TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    _c?.dispose();
+    super.dispose();
+  }
+  @override
    Widget build(BuildContext context) {
-     return Container();
-   }
+
+    cyclesMeasure() {
+
+      DateTime now = DateTime.now();
+
+      var first = now.add(new Duration(minutes: 90 + 14));
+      cycles['firstCycle'] = first.toIso8601String().substring(11, 16);
+
+      var second = now.add(new Duration(minutes: 180 + 14));
+      cycles['secondCycle'] = second.toIso8601String().substring(11, 16);
+
+      var third = now.add(new Duration(minutes: 270 + 14));
+      cycles['thirdCycle'] = third.toIso8601String().substring(11, 16);
+
+      var forth = now.add(new Duration(minutes: 360 + 14));
+      cycles['forthCycle'] = forth.toIso8601String().substring(11, 16);
+
+      var fifth = now.add(new Duration(minutes: 450 + 14));
+      cycles['fifthCycle'] = fifth.toIso8601String().substring(11, 16);
+
+      var sixth = now.add(new Duration(minutes: 540 + 14));
+      cycles['sixthCycle'] = sixth.toIso8601String().substring(11, 16);
+
+    }
+
+
+  }
  }
 
 
---
+++ lib/strings.dart
@@ -0,0 +1,10 @@
+
+class Strings{
+  static String oneCycle = 'One cycle: ';
+  static String twoCycles = 'Two cycles: ';
+  static String threeCycles = 'Three cycles: ';
+  static String fourCycles = 'Four cycles: ';
+  static String fiveCycles = 'Five cycles: ';
+  static String sixCycles = 'Six cycles: ';
+
+}
